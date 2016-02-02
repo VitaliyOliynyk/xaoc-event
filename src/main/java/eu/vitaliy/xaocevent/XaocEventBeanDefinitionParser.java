12897a004e32128a1f5785a21fbfd69783926e52
voliynyk <voliynyk@pzu.pl>
2016-02-02 13:54:53 +0100

    Add licence headers

--- src/main/java/eu/vitaliy/xaocevent/XaocEventBeanDefinitionParser.java
+++ src/main/java/eu/vitaliy/xaocevent/XaocEventBeanDefinitionParser.java
@@ -3,23 +3,39 @@ package eu.vitaliy.xaocevent;
 import eu.vitaliy.xaocevent.aspect.ObservableAspect;
 import eu.vitaliy.xaocevent.aspect.ObserverAspect;
 import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
-import org.springframework.aop.target.ThreadLocalTargetSource;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
-import org.springframework.beans.factory.support.*;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 /**
- * Created by IntelliJ IDEA.
- * User: xaoc
- * Date: 16.05.11
- * Time: 12:56
- * To change this template use File | Settings | File Templates.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author Vitaliy Oliynyk
  */
+
 public class XaocEventBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
     private final static String
             EVENT_QUEUE_CLASS_NAME = "eu.vitaliy.xaocevent.EventQueue",
